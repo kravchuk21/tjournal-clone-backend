033b8c9348e180901e5885d6f7ee9512af77c95d
kravchuk21 <krauchukvlad@gmail.com>
2021-10-15 00:55:21 +0300

    Урок 11-12

--- src/app.module.ts
+++ src/app.module.ts
@@ -15,7 +15,7 @@ import { AuthModule } from './auth/auth.module';
     TypeOrmModule.forRoot({
       type: 'postgres',
       host: 'localhost',
-      port: 5432,
+      port: 5433,
       username: 'postgres',
       password: 'password',
       database: 'tjournal',
